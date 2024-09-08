01e084cf1a920b27372bd3b455be11fba19ae82b
albert-bpc <atang@beachpointcapital.com>
2024-09-07 21:19:02 -0700

    clean up izbiki code

--- docsum.py
+++ docsum.py
@@ -1,3 +1,6 @@
+import os
+from groq import Groq
+import argparse
 
 def split_document_into_chunks(text):
     r'''
@@ -21,19 +24,32 @@ def split_document_into_chunks(text):
     '''
     return text.split('\n\n')
 
+def summarize(client, text):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                'role': 'system',
+                'content': (
+                    'Summarize the input text below. '
+                    'Limit the summary to 1 paragraph.'
+                    'Just output the summary do not return any commentary or other remarks.'
+                )
+            },
+            {
+                "role": "user",
+                "content": text,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    summary = chat_completion.choices[0].message.content
+    return summary
 
 if __name__ == '__main__':
-
-    import os
-    from groq import Groq
-
-    # parse command line args
-    import argparse
+    # parse input file
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     args = parser.parse_args()
-
-# line 7+8 => args.filename will contain the first string after program name on command line
 
     client = Groq(
         # This is the default and can be omitted
@@ -43,26 +59,5 @@ if __name__ == '__main__':
     with open(args.filename) as f:
         text = f.read()
 
-    '''
-    We need to call the split_document_into_chunks on text.
-    Then for each paragraph in the output list,
-    call the LLM code below to summarize it.
-    Put the summary into a new list.
-    Concatenate that new list into one smaller document.
-    Recall the LLM code below on the new smaller document.
-    '''
-
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                'role': 'system',
-                'content': 'Summarize the input text below.  Limit the summary to 1 paragraph and use a 1st grade reading level.',
-            },
-            {
-                "role": "user",
-                "content": text,
-            }
-        ],
-        model="llama3-8b-8192",
-    )
-    print(chat_completion.choices[0].message.content)
+    summary = summarize(client, text)
+    print(summary)
